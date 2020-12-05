862bf67f0d2e8f65d494cc4a4e6a3445639db4ba
Ash M <ashmos17@gmail.com>
2020-12-05 13:42:46 -0800

    HW19-v2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Side NavBar
 function openNav() {
     document.getElementById("mySidenav").style.width = "700px";
   }
@@ -5,3 +6,23 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  
+// Anchor Scroll
+$(document).ready(function() {
+
+  var scrollLink = $('.scroll');
+
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1100);
+  });
+
+})
+// AOS Animations
+AOS.init({
+  duration: 1200,
+})
+
